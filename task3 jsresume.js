b440c8583b7b01554acbd8d0cab8ee635bfe501a
sahil <sahilshinde1711@gmail.com>
2024-09-01 13:47:38 +0530

    uploaded task3

---
+++ task3 jsresume.js
@@ -0,0 +1,41 @@
+var resume={
+    name:"Sahil Shinde",
+    emailid:"sahilshinde1711@gmail.com",
+    mobno:9324426832,
+    city:"Mulund,Mumbai",
+    Education:[
+        {
+            college:"S.M.Shetty College of science,commerce and management studies",
+            course:"Bachelor of Science in Information Technology",
+             grade:"6.00 CGPA",
+            duration:"July 2019-July 2024",
+
+        },
+        {
+
+            college:"V.P.M.RZ Shah College",
+            stream:"Science (HSC)",
+            Percentage:"69.50%",
+            duration:"JULY-2017-JULY-2019",
+        },
+    ],
+    Skills:[
+        {
+            programmingLanguages: ["HTML", "CSS", "Javascript"],
+        },
+        
+      ],
+    projects:[
+        {
+            title:"shinde shoes wear",
+            languages:["html,css,js"],
+        },
+    ],
+
+}
+   
+    
+    
+console.log(resume)    
+        
+
